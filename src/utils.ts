b3f089a70a919c4f93b865f7eadb474131b47d86
许圣峰 <kricsleo@163.com>
2023-05-08 16:46:59 +0800

    chore: support text in spinner

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { dots, weather } from 'cli-spinners'
+import { moon } from 'cli-spinners'
 
 interface SpinnerOptions {
   animation: { 
@@ -7,17 +7,20 @@ interface SpinnerOptions {
   }
 }
 export class Spinner {
+  private text?: string
   private animation: SpinnerOptions['animation']
   private animationIdx = 0
   private timer: NodeJS.Timeout | null = null
   private stream = process.stderr
+  private isCursorHidden = false
   constructor(options?: SpinnerOptions) {
-    this.animation = options?.animation || dots
+    this.animation = options?.animation || moon
   }
   start(text?: string) {
-    if(this.timer) {
+    if(this.timer && this.text === text) {
       return
     }
+    this.text = text
     this.hideCursor()
     this.animate()
   }
@@ -28,29 +31,44 @@ export class Spinner {
     this.showCursor()
   }
   private animate() {
-    const frame = this.animation.frames[this.animationIdx % this.animation.frames.length]
+    if(this.timer) {
+      clearTimeout(this.timer)
+    }
+    const animationFrame = this.animation.frames[this.animationIdx % this.animation.frames.length]
+    const frame = animationFrame + this.text || '' + '\n'
     // TODO: process.stderr usage
     this.stream.cursorTo(0)
     this.stream.write(frame)
     this.animationIdx ++
     this.timer = setTimeout(() => this.animate(), this.animation.interval)
   }
-  showCursor() {
+  private showCursor() {
     // If the stream is connected to a TTY (terminal) device.
-    if(!this.stream.isTTY) {
+    if(!this.isCursorHidden || !this.stream.isTTY) {
       return
     }
+    // https://github.com/sindresorhus/cli-cursor/blob/main/index.js#L14
     this.stream.write('\u001B[?25h')
+    this.isCursorHidden = true
   }
-  hideCursor() {
+  private hideCursor() {
     // If the stream is connected to a TTY (terminal) device.
-    if(!this.stream.isTTY) {
+    if(this.isCursorHidden || !this.stream.isTTY) {
       return
     }
+    // https://github.com/sindresorhus/cli-cursor/blob/main/index.js#L24
     this.stream.write('\u001B[?25l')
+    this.isCursorHidden = false
   }
 }
 
-const spinner = new Spinner({ animation: weather })
+// const spinner = new Spinner()
 
-spinner.start()
+// spinner.start('hello')
+
+// setTimeout(() => {
+//   spinner.start('Got!')
+//   setTimeout(() => {
+//     spinner.stop()
+//   }, 3000)
+// }, 3000)
